7cd4c87191ef3673b0a1ede572e0ab09ae717cbc
Gabriel Libardi <gabriel_libardi@hotmail.com>
2026-06-14 22:09:21 -0300

    saving the preprocessed raw files

--- src/main.py
+++ src/main.py
@@ -3,6 +3,9 @@ from pathlib import Path
 from pdf_reader import read_all_pdfs
 from preprocessing import clean_text
 
+OUTPUT_DIR = Path("../data/processed")
+OUTPUT_DIR.mkdir(exist_ok=True)
+
 
 def main():
     documents = read_all_pdfs(Path("../data/raw"))
@@ -11,9 +14,11 @@ def main():
 
     for name, text in documents.items():
         cleaned = clean_text(text)
-
-        print("\n" + name)
-        print(cleaned[:500])
+        
+        (OUTPUT_DIR / f"{name}.txt").write_text(
+            cleaned,
+            encoding="utf-8"
+        )
 
 
 if __name__ == "__main__":
